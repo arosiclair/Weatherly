b25f85e67f13675a9240cae542211c85cece711c
arosiclair <arosiclair@gmail.com>
2015-08-08 02:28:21 -0400

    Changed the Placeholder Fragment to use the city_fragment layout

--- app/src/main/java/com/rosiclair/andrew/weatherly/ui/MainActivity.java
+++ app/src/main/java/com/rosiclair/andrew/weatherly/ui/MainActivity.java
@@ -89,8 +89,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            // Return a CityFragment (defined as a static inner class below).
+            return CityFragment.newInstance(position + 1);
         }
 
         @Override
@@ -117,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class CityFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
          * fragment.
@@ -128,21 +128,21 @@ public class MainActivity extends AppCompatActivity {
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
+        public static CityFragment newInstance(int sectionNumber) {
+            CityFragment fragment = new CityFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
             fragment.setArguments(args);
             return fragment;
         }
 
-        public PlaceholderFragment() {
+        public CityFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_city , container, false);
             return rootView;
         }
     }
