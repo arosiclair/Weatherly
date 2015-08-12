2aee8ae119b6f9627f2d3f547e48b1099a04a4d6
arosiclair <arosiclair@gmail.com>
2015-08-12 01:56:48 -0400

    Implemented event and error handling methods

--- app/src/main/java/com/rosiclair/andrew/weatherly/ui/PlayServicesEventHandler.java
+++ app/src/main/java/com/rosiclair/andrew/weatherly/ui/PlayServicesEventHandler.java
@@ -1,8 +1,17 @@
 package com.rosiclair.andrew.weatherly.ui;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
@@ -10,8 +19,21 @@ import com.google.android.gms.common.api.GoogleApiClient;
  */
 public class PlayServicesEventHandler implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
-    public PlayServicesEventHandler(){
+    private GoogleApiClient mGoogleApiClient;
+    //The activity associated with the GoogleApiClient
+    private AppCompatActivity mActivity;
 
+    // Request code to use when launching the resolution activity
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    // Unique tag for the error dialog fragment
+    private static final String DIALOG_ERROR = "dialog_error";
+    // Bool to track whether the app is already resolving an error
+    public boolean mResolvingError = false;
+    public static final String STATE_RESOLVING_ERROR = "resolving_error";
+
+    public PlayServicesEventHandler(AppCompatActivity activity, GoogleApiClient googleApiClient){
+        mActivity = activity;
+        mGoogleApiClient = googleApiClient;
     }
 
     @Override
@@ -33,5 +55,73 @@ public class PlayServicesEventHandler implements GoogleApiClient.ConnectionCallb
         // may occur while attempting to connect with Google.
         //
         // More about this in the 'Handle Connection Failures' section.
+
+        if(mResolvingError){
+            // Already attempting to resolve an error.
+            return;
+        }else if(result.hasResolution()){
+            try {
+                mResolvingError = true;
+                result.startResolutionForResult(mActivity, REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                // There was an error with the resolution intent. Try again.
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    /* Creates a dialog for an error message */
+    private void showErrorDialog(int errorCode) {
+        // Create a fragment for the error dialog
+        ErrorDialogFragment dialogFragment = new ErrorDialogFragment(this);
+        // Pass the error that should be displayed
+        Bundle args = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(mActivity.getSupportFragmentManager(), "errordialog");
+    }
+
+    /* Called from ErrorDialogFragment when the dialog is dismissed. */
+    public void onDialogDismissed() {
+        mResolvingError = false;
+    }
+
+    /* A fragment to display an error dialog */
+    public static class ErrorDialogFragment extends DialogFragment {
+
+        PlayServicesEventHandler mPlayServicesEventHandler;
+
+        public ErrorDialogFragment(){}
+
+        @SuppressLint("ValidFragment")
+        public ErrorDialogFragment(PlayServicesEventHandler playServicesEventHandler) {
+            mPlayServicesEventHandler = playServicesEventHandler;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Get the error code and retrieve the appropriate dialog
+            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
+            return GooglePlayServicesUtil.getErrorDialog(errorCode,
+                    this.getActivity(), REQUEST_RESOLVE_ERROR);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            mPlayServicesEventHandler.onDialogDismissed();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_RESOLVE_ERROR) {
+            mResolvingError = false;
+            if (resultCode == Activity.RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if (!mGoogleApiClient.isConnecting() &&
+                        !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
     }
 }
