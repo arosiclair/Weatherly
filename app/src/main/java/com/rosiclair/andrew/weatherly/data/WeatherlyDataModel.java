1f757dacaf2107fc426a4a029a46fd7ac8fec785
arosiclair <arosiclair@gmail.com>
2015-08-13 19:46:45 -0400

    Added WeatherlyDataModel that manages WeatherlyCities

---
+++ app/src/main/java/com/rosiclair/andrew/weatherly/data/WeatherlyDataModel.java
@@ -0,0 +1,31 @@
+package com.rosiclair.andrew.weatherly.data;
+
+import android.location.Location;
+
+import java.util.ArrayList;
+
+/**
+ * Created by arosi on 8/13/2015.
+ */
+public class WeatherlyDataModel {
+
+    //The collection of WeatherlyCities that will be used to update the views
+    private WeatherlyCity currentLocation;
+    private ArrayList<WeatherlyCity> cities;
+
+    public WeatherlyDataModel(){
+
+    }
+
+    public WeatherlyCity getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public ArrayList<WeatherlyCity> getCities() {
+        return cities;
+    }
+
+    public void updateCurrentLocation(Location lastKnown){
+
+    }
+}
