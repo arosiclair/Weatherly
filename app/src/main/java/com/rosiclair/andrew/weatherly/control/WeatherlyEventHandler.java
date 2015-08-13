87c26e0718abae3a02d58fb655a7f2a1db6952e3
arosiclair <arosiclair@gmail.com>
2015-08-13 19:47:13 -0400

    Added WeatherlyEventHandler

---
+++ app/src/main/java/com/rosiclair/andrew/weatherly/control/WeatherlyEventHandler.java
@@ -0,0 +1,13 @@
+package com.rosiclair.andrew.weatherly.control;
+
+import android.location.Location;
+
+/**
+ * Created by arosi on 8/13/2015.
+ */
+public class WeatherlyEventHandler {
+
+    public void onLocationUpdate(Location lastKnown){
+
+    }
+}
