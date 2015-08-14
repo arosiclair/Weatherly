ab208e2804a3d10db84d2a8af5015043e9a29318
arosiclair <arosiclair@gmail.com>
2015-08-14 02:54:20 -0400

    Partially implemented onLocationUpdate

--- app/src/main/java/com/rosiclair/andrew/weatherly/control/WeatherlyEventHandler.java
+++ app/src/main/java/com/rosiclair/andrew/weatherly/control/WeatherlyEventHandler.java
@@ -7,9 +7,15 @@ import com.rosiclair.andrew.weatherly.data.WeatherlyCity;
 import com.rosiclair.andrew.weatherly.data.WeatherlyDataModel;
 import com.survivingwithandroid.weather.lib.WeatherClient;
 import com.survivingwithandroid.weather.lib.WeatherConfig;
+import com.survivingwithandroid.weather.lib.exception.WeatherLibException;
 import com.survivingwithandroid.weather.lib.exception.WeatherProviderInstantiationException;
+import com.survivingwithandroid.weather.lib.model.CurrentWeather;
 import com.survivingwithandroid.weather.lib.model.Weather;
 import com.survivingwithandroid.weather.lib.provider.openweathermap.OpenweathermapProviderType;
+import com.survivingwithandroid.weather.lib.request.WeatherRequest;
+
+import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by arosi on 8/13/2015.
@@ -27,7 +33,38 @@ public class WeatherlyEventHandler {
     }
 
     public void onLocationUpdate(Location lastKnown){
+        final WeatherlyCity currentLocation = mDataModel.getCurrentLocation();
 
+        double latitude = lastKnown.getLatitude();
+        double longitude = lastKnown.getLongitude();
+
+        WeatherRequest request = new WeatherRequest(longitude, latitude);
+
+        //Get current condition using an anonymous listener
+        mWeatherClient.getCurrentCondition(request, new WeatherClient.WeatherEventListener(){
+            @Override
+            public void onWeatherRetrieved(CurrentWeather cWeather) {
+                Weather weather = cWeather.weather;
+
+                //Current weather conditions
+                currentLocation.setName(weather.location.getCity());
+                currentLocation.setCurrentTemp((int) weather.temperature.getTemp());
+                currentLocation.setWind((int) weather.wind.getSpeed());
+                currentLocation.setHumidity((int) weather.currentCondition.getHumidity());
+                currentLocation.setVisibility((int) weather.currentCondition.getVisibility());
+                currentLocation.setLastUpdateTime(TimeUnit.NANOSECONDS.toMinutes(System.nanoTime()));
+            }
+
+            @Override
+            public void onWeatherError(WeatherLibException t) {
+            // Something went wrong maybe we should inform the user
+            }
+
+            @Override
+            public void onConnectionError(Throwable t) {
+            // There was a connection error
+            }
+        });
     }
 
     public void buildWeatherClient(){
