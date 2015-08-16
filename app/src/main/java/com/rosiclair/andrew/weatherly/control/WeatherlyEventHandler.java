c3d797725096b2c2b2769b88060ba6fa55d16105
arosiclair <arosiclair@gmail.com>
2015-08-16 04:00:39 -0400

    Replaced use of Weatherlib for ForecastIO lib

--- app/src/main/java/com/rosiclair/andrew/weatherly/control/WeatherlyEventHandler.java
+++ app/src/main/java/com/rosiclair/andrew/weatherly/control/WeatherlyEventHandler.java
@@ -3,29 +3,24 @@ package com.rosiclair.andrew.weatherly.control;
 import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 
+import com.github.dvdme.ForecastIOLib.FIOCurrently;
+import com.github.dvdme.ForecastIOLib.ForecastIO;
 import com.rosiclair.andrew.weatherly.data.WeatherlyCity;
 import com.rosiclair.andrew.weatherly.data.WeatherlyDataModel;
-import com.survivingwithandroid.weather.lib.WeatherClient;
-import com.survivingwithandroid.weather.lib.WeatherConfig;
-import com.survivingwithandroid.weather.lib.exception.WeatherLibException;
-import com.survivingwithandroid.weather.lib.exception.WeatherProviderInstantiationException;
-import com.survivingwithandroid.weather.lib.model.CurrentWeather;
-import com.survivingwithandroid.weather.lib.model.Weather;
-import com.survivingwithandroid.weather.lib.provider.openweathermap.OpenweathermapProviderType;
-import com.survivingwithandroid.weather.lib.request.WeatherRequest;
+import com.rosiclair.andrew.weatherly.data.WeatherlyDayForecast;
 
 import java.util.GregorianCalendar;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by arosi on 8/13/2015.
+ * An Event Handler that handles all UI and refresh events
  */
 public class WeatherlyEventHandler {
 
     private AppCompatActivity mActivity;
-
-    private WeatherClient mWeatherClient;
     private WeatherlyDataModel mDataModel;
+
+    private ForecastIO mFIO;
 
     public WeatherlyEventHandler(AppCompatActivity activity, WeatherlyDataModel dataModel){
         mActivity = activity;
@@ -33,59 +28,26 @@ public class WeatherlyEventHandler {
     }
 
     public void onLocationUpdate(Location lastKnown){
-        final WeatherlyCity currentLocation = mDataModel.getCurrentLocation();
+        WeatherlyCity currentLocation = mDataModel.getCurrentLocation();
+        WeatherlyDayForecast todaysForecast = currentLocation.getTodaysForecast();
 
         double latitude = lastKnown.getLatitude();
         double longitude = lastKnown.getLongitude();
 
-        WeatherRequest request = new WeatherRequest(longitude, latitude);
+        //Initialize a new ForecastIO for each new forecast
+        mFIO = new ForecastIO("147011b1efd5b4f5db2beafa790a82b6");
+        mFIO.setUnits(ForecastIO.UNITS_US);
+        mFIO.setExcludeURL("minutely");
+        mFIO.getForecast("" + latitude, "" + longitude);
 
-        //Get current condition using an anonymous listener
-        mWeatherClient.getCurrentCondition(request, new WeatherClient.WeatherEventListener(){
-            @Override
-            public void onWeatherRetrieved(CurrentWeather cWeather) {
-                Weather weather = cWeather.weather;
+        //Retrieve current conditions
+        FIOCurrently currently = new FIOCurrently(mFIO);
+        currentLocation.setCurrentTemp((int) (currently.get().temperature() + 0.5));
+        currentLocation.setFeelsLike((int) (currently.get().apparentTemperature() + 0.5));
+        currentLocation.setWind((int) (currently.get().windSpeed() + 0.5));
+        currentLocation.setHumidity((int) (currently.get().humidity() + 0.5));
+        currentLocation.setVisibility((int) (currently.get().visibility() + 0.5));
 
-                //Current weather conditions
-                currentLocation.setName(weather.location.getCity());
-                currentLocation.setCurrentTemp((int) weather.temperature.getTemp());
-                currentLocation.setWind((int) weather.wind.getSpeed());
-                currentLocation.setHumidity((int) weather.currentCondition.getHumidity());
-                currentLocation.setVisibility((int) weather.currentCondition.getVisibility());
-                currentLocation.setLastUpdateTime(TimeUnit.NANOSECONDS.toMinutes(System.nanoTime()));
-            }
-
-            @Override
-            public void onWeatherError(WeatherLibException t) {
-            // Something went wrong maybe we should inform the user
-            }
-
-            @Override
-            public void onConnectionError(Throwable t) {
-            // There was a connection error
-            }
-        });
-    }
-
-    public void buildWeatherClient(){
-        WeatherClient.ClientBuilder builder = new WeatherClient.ClientBuilder();
-
-        //Init and setup config
-        WeatherConfig config = new WeatherConfig();
-        config.ApiKey = "db1ce9e4cc58d1f6f010471c33d68479";
-        config.unitSystem = WeatherConfig.UNIT_SYSTEM.I;
-        config.lang = "en";
-        config.maxResult = 5; // Max number of cities retrieved
-        config.numDays = 1; // Max num of days in the forecast
-
-        try {
-            mWeatherClient = builder.attach(mActivity)
-                    .provider(new OpenweathermapProviderType())
-                    .httpClient(com.survivingwithandroid.weather.lib.client.volley.WeatherClientDefault.class)
-                    .config(config)
-                    .build();
-        } catch (WeatherProviderInstantiationException e) {
-            e.printStackTrace();
-        }
+        //TODO Retrieve today's forecast
     }
 }
